abe21ce7788e1ea304f5ce3fee575984ee9f889d
syouzen <seungyeon.choi1026@gmail.com>
2019-05-01 15:09:20 +0900

    [feat] Add Bipartite Matching Example
    - same as title.

---
+++ src/python/2188/main.py
@@ -0,0 +1,28 @@
+# Date : 2017.11.10
+# Author : seungyeon choi
+# 2188 (축사배정)
+
+ans = 0
+N, M = map(int, input().split())
+adj = [[] for _ in range(M)]
+cow = [-1 for _ in range(N)]
+house = [-1 for _ in range(M)]
+for i in range(N):
+    S =  list(map(int, input().split()))
+    for j in range(1, len(S)):
+        adj[i].append(S[j]-1)
+for j in range(N):
+    visit = [0 for _ in range(N)]
+    def dfs(i):
+        if visit[i] :
+            return False
+        visit[i] = 1
+        for j in range(len(adj[i])):
+            if house[adj[i][j]] == -1 or dfs(house[adj[i][j]]):
+                cow[i] = adj[i][j]
+                house[adj[i][j]] = i
+                return True
+        return False
+    if dfs(j):
+        ans += 1
+print (ans)
